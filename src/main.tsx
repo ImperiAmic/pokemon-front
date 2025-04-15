e5ee58108df98c99918987cc0412275f18bbf219
Raimon <raimon.txollo@gmail.com>
2025-04-15 19:37:03 +0200

    Add fonts and reset body, h1 and img styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import "@fontsource/permanent-marker/index.css";
+import "@fontsource-variable/roboto/index.css";
+import App from "./components/App/App";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
