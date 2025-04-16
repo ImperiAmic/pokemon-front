122dba309df019d992607862ed97b5e056a0c7a5
Raimon <raimon.txollo@gmail.com>
2025-04-16 17:18:23 +0200

    Install react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import "@fontsource/permanent-marker/index.css";
 import "@fontsource-variable/roboto/index.css";
 import App from "./components/App/App";
@@ -7,6 +8,8 @@ import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
