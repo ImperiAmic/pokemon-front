769c42f3a0e84c4a711a240009fd8d68ab84bc6b
Raimon <raimon.txollo@gmail.com>
2025-04-16 19:51:53 +0200

    Add AppRouter component

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "@fontsource/permanent-marker/index.css";
 import "@fontsource-variable/roboto/index.css";
-import App from "./components/App/App";
 import "./styles/styles.css";
+import AppRouter from "./router/AppRouter";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRouter />
     </BrowserRouter>
   </StrictMode>,
 );
