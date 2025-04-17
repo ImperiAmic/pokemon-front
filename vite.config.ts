61fa2c6c65275e88aeede4a5fd666c038405192b
Raimon <raimon.txollo@gmail.com>
2025-04-17 23:46:23 +0200

    Change default port to 4001

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 4001 },
   build: { target: "esnext" },
   plugins: [react()],
   test: {
