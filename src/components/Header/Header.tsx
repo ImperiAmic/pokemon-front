b3fc3db949ccf5317cc38060226212fbcfa2d9f4
Raimon <raimon.txollo@gmail.com>
2025-04-17 11:50:07 +0200

    Refactor styles from App, Header and Navigator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,19 +5,21 @@ const Header: React.FC = () => {
   return (
     <header className="main-header">
       <div className="main-header__container">
-        <div className="main-header__text-container">
-          <h1 className="main-header__title">Pokédex</h1>
-          <span className="main-header__claim">Gotta manage 'em all!</span>
+        <div className="main-header__brand-container">
+          <div className="main-header__text-container">
+            <h1 className="main-header__title">Pokédex</h1>
+            <span className="main-header__claim">Gotta manage 'em all!</span>
+          </div>
+          <img
+            className="main-header__image"
+            src="/mew.svg"
+            alt="Pokémon Mew joyfuly floating"
+            height={100}
+            width={92}
+          />
         </div>
-        <img
-          className="main-header__image"
-          src="/mew.svg"
-          alt="Pokémon Mew joyfuly floating"
-          height={100}
-          width={92}
-        />
+        <Navigator />
       </div>
-      <Navigator />
     </header>
   );
 };
