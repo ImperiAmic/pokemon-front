bc3e33fedd0ecd2b5046b9eafc1dea5d30115b78
Raimon <raimon.txollo@gmail.com>
2025-04-19 17:30:07 +0200

    Add AddPokemonPage component, styles and test

---
+++ src/pokemon/pages/AddPokemonPage/AddPokemonPage.tsx
@@ -0,0 +1,15 @@
+import PokemonForm from "../../component/PokemonForm/PokemonForm";
+import "./AddPokemonPage.css";
+
+const AddPokemonPage: React.FC = () => {
+  return (
+    <main className="main-container">
+      <div className="form-container">
+        <h2 className="form__title">Add a Pókemon!</h2>
+        <PokemonForm />
+      </div>
+    </main>
+  );
+};
+
+export default AddPokemonPage;
